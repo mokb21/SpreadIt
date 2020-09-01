1553c1084a240164d319215a240f62bd887bc7fa
Mohamed Kabbani <kabbani13666@outlook.com>
2020-09-01 14:11:15 +0300

    fixing signalR hub

--- SpreadIt/SpreadIt.API/Hubs/SpreadItHub.cs
+++ SpreadIt/SpreadIt.API/Hubs/SpreadItHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,20 @@ namespace SpreadIt.API.Hubs
         public async Task SendMessage()
         {
             if (Clients != null)
-                await Clients.All.SendAsync("ReceiveMessage", "New Post Added !!");
+                await Clients.Group("SignalR Users").SendAsync("ReceiveMessage", "New Post Added !!");
         }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+
     }
 }
